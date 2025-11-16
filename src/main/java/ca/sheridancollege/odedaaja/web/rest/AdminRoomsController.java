64a5b57cfd80d913e34286c4cc30e26a758707fd
Mann Mehta <134656791+mehmannj@users.noreply.github.com>
2025-11-16 12:16:49 -0500

    Add CORS support for Netlify app

--- src/main/java/ca/sheridancollege/odedaaja/web/rest/AdminRoomsController.java
+++ src/main/java/ca/sheridancollege/odedaaja/web/rest/AdminRoomsController.java
@@ -14,7 +14,7 @@ import ca.sheridancollege.odedaaja.Locker.repo.RoomAssignmentRepository;
 
 @RestController
 @RequestMapping("/api/admin/rooms")
-@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173"}, allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173", "https://instimanage.netlify.app" }, allowCredentials = "true")
 @RequiredArgsConstructor
 public class AdminRoomsController {
 
