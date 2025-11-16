c268da847e0217f5539fb4b2aa69dd427ae1f8fb
Mann Mehta <134656791+mehmannj@users.noreply.github.com>
2025-11-16 12:18:16 -0500

    Add new CORS origin for production environment

--- src/main/java/ca/sheridancollege/odedaaja/web/rest/Prof_RoomBookController.java
+++ src/main/java/ca/sheridancollege/odedaaja/web/rest/Prof_RoomBookController.java
@@ -26,7 +26,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/profRoomBook")
-@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173"})
+@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173", "https://instimanage.netlify.app" })
 
 public class Prof_RoomBookController {
     private final Prof_RoomBook_Service profRoomBookService;
