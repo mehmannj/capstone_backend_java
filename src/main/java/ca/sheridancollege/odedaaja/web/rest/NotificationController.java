d8e38ffe1a3bc41a6298ad41965a28b5d3295ade
Mann Mehta <134656791+mehmannj@users.noreply.github.com>
2025-11-16 12:18:06 -0500

    Add new origin to CrossOrigin annotation

--- src/main/java/ca/sheridancollege/odedaaja/web/rest/NotificationController.java
+++ src/main/java/ca/sheridancollege/odedaaja/web/rest/NotificationController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/notifications")
-@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173"}, allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173", "https://instimanage.netlify.app" }, allowCredentials = "true")
 @RequiredArgsConstructor
 public class NotificationController {
 
