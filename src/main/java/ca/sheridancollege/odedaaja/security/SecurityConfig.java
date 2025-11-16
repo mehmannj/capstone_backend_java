ea18e88726b6b95fd1a89e0559b83b0b7e5ca541
Mann Mehta <134656791+mehmannj@users.noreply.github.com>
2025-11-16 11:45:09 -0500

    Add new allowed origin to CORS configuration

--- src/main/java/ca/sheridancollege/odedaaja/security/SecurityConfig.java
+++ src/main/java/ca/sheridancollege/odedaaja/security/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://127.0.0.1:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://127.0.0.1:5173","https://instimanage.netlify.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true); // needed since you're using withCredentials
