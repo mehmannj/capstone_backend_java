1b9aefa44a978f6b3abaf24461cd1cf1f5b5ae54
Mann Mehta <134656791+mehmannj@users.noreply.github.com>
2025-11-16 12:17:00 -0500

    Add new origin to CrossOrigin annotation

--- src/main/java/ca/sheridancollege/odedaaja/web/rest/BookedRoomController.java
+++ src/main/java/ca/sheridancollege/odedaaja/web/rest/BookedRoomController.java
@@ -22,7 +22,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/BookedRoom")
-@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173"})
+@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173","https://instimanage.netlify.app" })
 
 public class BookedRoomController {
 
