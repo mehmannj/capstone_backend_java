d288de16c0c93c8fe70c8b07c6c3a773f240abd9
Mann Mehta <134656791+mehmannj@users.noreply.github.com>
2025-11-16 12:18:30 -0500

    Add new origin to CrossOrigin annotation

--- src/main/java/ca/sheridancollege/odedaaja/web/rest/ReceiptController.java
+++ src/main/java/ca/sheridancollege/odedaaja/web/rest/ReceiptController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/receipts")
-@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173"}, allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173", "https://instimanage.netlify.app" }, allowCredentials = "true")
 @RequiredArgsConstructor
 public class ReceiptController {
 
